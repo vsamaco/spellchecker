b1f7ef35adf474869b4636cb58ab7ca291e93c39
Vincent Samaco <vsamaco@gmail.com>
2013-01-15 18:47:35 -0800

    Implemented deletion logic

--- lib/spellchecker.rb
+++ lib/spellchecker.rb
@@ -24,8 +24,9 @@ class SpellChecker
   
   def known(words)
     known_words = Set.new
+    
     words.each do |word|
-      known_words << word if @dictionary.include?(word)
+      known_words << word if @dictionary.has_key?(word)
     end
     
     known_words
@@ -35,13 +36,18 @@ class SpellChecker
     suggestions = Set.new
     suggestions = known([word])
     suggestions = known(edit_by_one(word)) if suggestions.empty?
-    
+
     suggestions.max{ |a, b| @dictionary[a] <=> @dictionary[b] }
   end
   
   def edit_by_one(word)
-    deletes = Array.new
+    n = word.length
     
-    Set.new(deletes)
+    # create variation that removes character
+    deletion = (0...n).collect{ |i| word[0...i] + word[i+1..-1] }
+    
+    
+    puts "deletion: #{deletion.to_s}"
+    Set.new(deletion)
   end
 end
